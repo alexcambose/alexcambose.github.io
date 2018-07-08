9e02ee366170059b6dec1ca4bf048d2a7ba403f6
Alexandru Cambose <alexcambose@yahoo.com>
2018-07-08 21:11:00 +0300

    Svg image animation

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export const header = {
     shapesContainer: '#header-background',
-    shapesColor: ['red', 'rgb(212, 101, 0)', 'rgb(0, 204, 102)', 'rgb(51, 153, 255)', 'rgb(153, 102, 255)', 'rgb(255, 153, 51)', 'rgb(0, 51, 0)', 'black', 'gray', 'blue'],
+    shapesColorDark: ['#ffffff', '#fffa00', '#ff2167', '#26ff9d', '#ff966d', '#ff00fa', '#00ffd0', '#ff0000'], // when the background is dark
+    shapesColorLight: ['#000000', '#3cff5f', '#456d5c', '#0037ff','#19ba03', '#00d4ff', '#6b3470', '#af186e', '#d10202'], // when white
     shapeSize: 30, // n x n in px
     shapeStrokeWidth: 3, // px
     shapesCount: 10,
--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,8 +1,8 @@
 import { randomFromInterval } from './utils.js';
-import { header } from '../config';
+import { header as headerConfig } from '../config';
 
 export default () => {
-    const backgroundContainer = document.querySelector(header.shapesContainer);
+    const backgroundContainer = document.querySelector(headerConfig.shapesContainer);
     const title = document.querySelector('.header .title');
 
     const wh = window.innerHeight;
@@ -30,7 +30,7 @@ export default () => {
     ];
 
     let elements = [];
-    for (let i = 1; i <= header.shapesCount; i++) {
+    for (let i = 1; i <= headerConfig.shapesCount; i++) {
         const elementType = types[randomFromInterval(0, types.length - 1)];
         let posX = randomFromInterval(10, ww);
         let posY = randomFromInterval(10, wh);
@@ -46,8 +46,8 @@ export default () => {
 
         const svgContainer = document.createElementNS('http://www.w3.org/2000/svg', 'svg'); // create <svg/> container for the shape
         svgContainer.setAttribute('viewBox', `0 0 100 100`);
-        svgContainer.setAttribute('height', header.shapeSize);
-        svgContainer.setAttribute('width', header.shapeSize);
+        svgContainer.setAttribute('height', headerConfig.shapeSize);
+        svgContainer.setAttribute('width', headerConfig.shapeSize);
         svgContainer.style.transform = `rotate(${rotation}deg)`; // set random rotation from the beginning
         svgContainer.style.position = 'absolute';
         svgContainer.style.top = posY;
@@ -57,8 +57,8 @@ export default () => {
         Object.keys(elementType)
             .filter(e => e !== 'type')
             .forEach(e => svgElement.setAttribute(e, elementType[e]));
-        svgElement.style.stroke = header.shapesColor[randomFromInterval(0, header.shapesColor.length - 1)];
-        svgElement.style.strokeWidth = header.shapeStrokeWidth;
+        svgElement.style.stroke = headerConfig.shapesColorLight[randomFromInterval(0, headerConfig.shapesColorLight.length - 1)];
+        svgElement.style.strokeWidth = headerConfig.shapeStrokeWidth;
         svgElement.style.fill = 'transparent';
 
         if (elementType.type !== 'path') {
@@ -69,7 +69,8 @@ export default () => {
         backgroundContainer.appendChild(svgContainer); // add <svg/> to the container div
 
         elements.push({
-            element: svgContainer,
+            elementContainer: svgContainer,
+            element: svgElement,
             rotation,
             posX,
             posY,
@@ -81,18 +82,43 @@ export default () => {
         const mouseX = e.clientX;
         const mouseY = e.clientY;
         for (let i = 0; i < elements.length; i++) {
-            const {element, divider, rotation} = elements[i];
-            element.style.transform = `rotate(${rotation}deg) translate(${(mouseX - ww / 2) / divider}px, ${(mouseY - wh / 2) / divider}px)`;
+            const {elementContainer, divider, rotation} = elements[i];
+            elementContainer.style.transform = `rotate(${rotation}deg) translate(${(mouseX - ww / 2) / divider}px, ${(mouseY - wh / 2) / divider}px)`;
         }
     };
     document.addEventListener('mousemove', calcShapesPosition);
 
-
+    //Click event for mouse icon
     const mouseIcon = document.getElementById('mouse-icon');
     mouseIcon.addEventListener('click', () => {
         window.scrollTo({
             "behavior": "smooth",
             "top": wh
         });
+    });
+
+    //invert colors after user scrolled half of the viewport height
+    let isDarkened = false;
+    const check = () => {
+        const header = document.querySelector('.header');
+        if(window.scrollY > window.innerHeight/2 - 300 ) {
+            if(!isDarkened) {
+                header.classList.add('inverse');
+                elements.forEach(({element}) => {
+                    element.style.stroke = headerConfig.shapesColorDark[randomFromInterval(0, headerConfig.shapesColorDark.length - 1)]
+                });
+                isDarkened = true;
+            }
+        } else {
+            isDarkened = false;
+            elements.forEach(({element}) => {
+                element.style.stroke = headerConfig.shapesColorLight[randomFromInterval(0, headerConfig.shapesColorLight.length - 1)]
+            });
+            header.classList.remove('inverse');
+        }
+    };
+    check();
+    document.addEventListener('scroll', () => {
+        check();
     })
 };
