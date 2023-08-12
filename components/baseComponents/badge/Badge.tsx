a786f5d0d4c8e7588c1d8ab8c363358df933ede9
alexcambose <alexcambose@yahoo.com>
2023-08-12 14:43:22 +0800

    feat: optional badge size

--- components/baseComponents/badge/Badge.tsx
+++ components/baseComponents/badge/Badge.tsx
@@ -3,13 +3,14 @@ import { ReactNode } from 'react';
 
 export interface BadgeProps {
   children: ReactNode;
-  size: 'normal' | 'small';
+  size?: 'normal' | 'small';
 }
 export const Badge = ({ children, size = 'normal' }: BadgeProps) => {
   return (
     <span
       className={classNames(
-        'mb-2 mr-2 inline-block select-none rounded-full bg-gray-200 px-3 py-1 text-xs font-semibold text-gray-700',
+        'mb-2 mr-2 inline-block select-none rounded-full bg-gray-200 dark:bg-gray-800  dark:text-default-dark text-gray-600 font-semibold ',
+        { 'text-xs px-3 py-1': size === 'normal' },
         { 'text-xxs px-2 py-1': size === 'small' }
       )}
     >
