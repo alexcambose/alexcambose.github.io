24ac48aedd76b89089edaac49ae10a748539471a
alexcambose <alexcambose@yahoo.com>
2023-08-21 23:58:05 +0800

    feat: updated resume

--- app/data.ts
+++ app/data.ts
@@ -5,13 +5,13 @@ export const workTimelineData = [
     companyUrl: 'https://www.cultofcoders.com/',
     startDate: new Date('01.09.2018'),
     endDate: new Date('01.10.2019'),
-    description: `Worked on different outsourcing projects and companies, including a US startup.
+    description: `
+I've worked on different outsourcing projects and companies, including a US startup.
 - responsible for front-end and back-end web development
 - closely worked with clients in an agile environment to establish objectives and deliver incremental software updates
 - collaborate with the rest of the engineering team to design and launch new features
-- setup and manage development and production servers to ensure a good workflow and stability
-`,
-    techStack: ['React', 'SCSS', 'NodeJs', 'Typescript', 'GraphQL', 'AWS'],
+- setup and manage development and production servers to ensure a good workflow and stability`,
+    techStack: ['React', 'SCSS', 'NodeJs', 'Typescript', 'Meteor', 'GraphQL', 'AWS'],
     companyImageUrls: {
       smallSizeParams: {
         width: 50,
@@ -39,7 +39,7 @@ export const workTimelineData = [
     title: 'Senior Full-Stack Developer',
     companyName: 'MixDice',
     companyUrl: 'https://twitter.com/MixDiceCasino',
-    description: `Lead Developer for a cryptocurrency-based gaming platform.
+    description: `I've worked on a cryptocurrency-based gaming platform.
 Key Responsibilities:
 - led the development of the project
 - responsible for front-end and back-end web development
@@ -47,7 +47,7 @@ Key Responsibilities:
 - setup the infrastructure and ci/cd pipelines for the app
 - implemented Bitcoin and Ethereum deposit/withdrawal/wallet systems for users and administrators by setting up BCoin nodes (https://bcoin.io/) and Geth nodes (https://geth.ethereum.org/)
 - implemented a provably fair random generation system (https://medium.com/@alexcambose/provably-fair-system-in-javascript-6457e028d2aa)
-- implemented real-time websocket based communication for different parts of the app`,
+- implemented real-time websocket-based communication for different parts of the app`,
     startDate: new Date('01.10.2019'),
     endDate: new Date('01.06.2020'),
     techStack: [
@@ -62,6 +62,7 @@ Key Responsibilities:
       'Web3js',
       'Geth',
       'Bcoin',
+      'Ngnix',
     ],
     companyImageUrls: {
       smallSizeParams: {
@@ -83,13 +84,13 @@ Key Responsibilities:
   {
     title: 'Senior Full-Stack Developer',
     companyName: 'Instantoption',
-    description: `Worked on a decentralized binary options protocol.
+    description: `I've worked on a decentralized binary options protocol.
 Key Responsibilities:
 - led the frontend and backend development
-- achieved seamless integration between frontend and on-chain smart contracts
-- developed custom charting solutions
+- integrated the frontend with on-chain smart contracts
+- developed custom charting solutions for displaying user trades
 - implemented different authentication methods, both injected wallet and social login
-- setup the infrastructure and ci/cd pipelines
+- setup the infrastructure and CI/CD pipelines
 - conducted code reviews to ensure high code standards`,
     startDate: new Date('01.11.2020'),
     endDate: new Date('01.02.2021'),
@@ -128,7 +129,7 @@ Key Responsibilities:
     companyUrl: 'https://rolla.fi',
     startDate: new Date('01.09.2021'),
     endDate: new Date('01.09.2023'),
-    description: `Worked on different projects over time, DeFi crypto options exchange, DeFi option-based yield generation app, ERC 4337 based DeFi price prediction app using spreads and digital options.
+    description: `I've worked on different projects over time, DeFi crypto options exchange, DeFi option-based yield generation app, ERC 4337 based DeFi price prediction app using spreads and digital options.
 Key Responsibilities:
 - lead frontend developer
 - worked on diverse roles, frontend, backend, and mobile
