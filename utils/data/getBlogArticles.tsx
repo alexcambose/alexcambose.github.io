b0a2ba6f08fcccbb4ca0a6793ce0d122284c8890
alexcambose <alexcambose@yahoo.com>
2025-03-12 20:08:50 -0300

    fix: article images

--- utils/data/getBlogArticles.tsx
+++ utils/data/getBlogArticles.tsx
@@ -1,11 +1,18 @@
 import { Article } from '@/components/blog/types';
 import { JSDOM } from 'jsdom';
 
-function extractDescription(description: string): string {
+const extractDescription = (description: string): string => {
   const dom = new JSDOM(description);
   const snippetElement = dom.window.document.querySelector('.medium-feed-snippet');
   return snippetElement ? snippetElement.textContent || '' : '';
-}
+};
+
+const extractThumbnail = (description: string) => {
+  const dom = new JSDOM(description);
+  const snippetElement = dom.window.document.querySelector('.medium-feed-item img');
+  return snippetElement ? snippetElement.getAttribute('src') : '';
+};
+
 export const getBlogArticles = async () => {
   const res = await fetch(
     `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@alexcambose`
@@ -15,7 +22,7 @@ export const getBlogArticles = async () => {
     title: item.title,
     description: extractDescription(item.description),
     categories: item.categories.map((e: string) => e.charAt(0).toUpperCase() + e.slice(1)),
-    thumbnail: item.thumbnail,
+    thumbnail: extractThumbnail(item.description),
     publishedDate: item.pubDate,
     linkUrl: item.link,
   })) as Article[];
